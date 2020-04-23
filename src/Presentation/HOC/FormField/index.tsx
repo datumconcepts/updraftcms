3f29ee2a7a39f8ee412a03f45b3694428023e272
Brett Smith <brett.smith@datumconcepts.com>
2020-04-23 20:51:32 +1200

    filename changes

--- src/Presentation/HOC/FormField/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-class FormField extends React.Component {
-  public render() {
-    const { children } = this.props;
-    return <div style={{ padding: 18 }}>{children}</div>;
-  }
-}
-export default FormField;
