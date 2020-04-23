69e0487bb8820ff0df553d7ba42467db801a155b
Brett Smith <brett@pukekos.co.nz>
2020-04-23 13:36:29 +1200

    Feature/media objects (#6)
    * object model changes
    
    * object models
    
    Co-authored-by: Brett Smith <brett.smith@datumconcepts.com>

--- src/Store/MiddlewareSupport/data-persistence/index.ts
+++ src/Store/MiddlewareSupport/data-persistence/index.ts
@@ -7,40 +7,40 @@ import { IContentDocument, IObjectModel } from 'Types';
 
 
 export interface IDataPersistenceMiddleware {
-    DeleteObjectModel(deletedObjectModel: IObjectModel, objectModels: Map<string, IObjectModel>): void;
-    RequestObjectModels(): Map<string, IObjectModel>;
-    SaveObjectModel(savedObjectModel: IObjectModel, objectModels: Map<string, IObjectModel>): void;
-    DeleteContentDocument(deletedContentDocument: IContentDocument, contentDocuments: Map<string, IContentDocument>): void;
-    RequestContentDocuments(): Map<string, IContentDocument>;
-    SaveContentDocument(savedContentDocument: IContentDocument, contentDocuments: Map<string, IContentDocument>): void;
+    DeleteObjectModel(deletedObjectModel: IObjectModel, objectModels: Map<string, IObjectModel>): Promise<any>;
+    RequestObjectModels(): Promise<Map<string, IObjectModel>>;
+    SaveObjectModel(savedObjectModel: IObjectModel, objectModels: Map<string, IObjectModel>): Promise<any>;
+    DeleteContentDocument(deletedContentDocument: IContentDocument, contentDocuments: Map<string, IContentDocument>): Promise<any>;
+    RequestContentDocuments(): Promise<Map<string, IContentDocument>>;
+    SaveContentDocument(savedContentDocument: IContentDocument, contentDocuments: Map<string, IContentDocument>): Promise<any>;
 }
 
 export const middleware = (concreteClass: IDataPersistenceMiddleware) => {
     return (api: AppMiddlewareApi) =>
         (next: Dispatch<AnyAction>) =>
-            (action: KnownAppActions) => {
+            async (action: KnownAppActions) => {
                 switch (action.type) {
                     case "DELETE_OBJECT_MODEL":
-                        concreteClass.DeleteObjectModel(action.deletedObjectModel, action.objectModels);
-                        break;
+                        await concreteClass.DeleteObjectModel(action.deletedObjectModel, action.objectModels);
+                        return next(action);
                     case "REQUEST_OBJECT_MODELS":
-                        const objectModels = concreteClass.RequestObjectModels();
-                        api.dispatch({ type: 'RECEIVE_OBJECT_MODELS', objectModels })
-                        break;
+                        const objectModels = await concreteClass.RequestObjectModels();
+                        return next({ type: 'RECEIVE_OBJECT_MODELS', objectModels })
                     case "SAVE_OBJECT_MODEL":
-                        concreteClass.SaveObjectModel(action.savedObjectModel, action.objectModels);
-                        break;
+                        await concreteClass.SaveObjectModel(action.savedObjectModel, action.objectModels);
+                        return next(action);
                     case "DELETE_CONTENT_DOCUMENT":
-                        concreteClass.DeleteContentDocument(action.deletedContentDocument, action.contentDocuments);
-                        break;
+                        await concreteClass.DeleteContentDocument(action.deletedContentDocument, action.contentDocuments);
+                        return next(action);
                     case "REQUEST_CONTENT_DOCUMENTS":
-                        const contentDocuments = concreteClass.RequestContentDocuments();
+                        const contentDocuments = await concreteClass.RequestContentDocuments();
                         api.dispatch({ type: 'RECEIVE_CONTENT_DOCUMENTS', contentDocuments });
-                        break;
+                        return next(action);
                     case "SAVE_CONTENT_DOCUMENT":
-                        concreteClass.SaveContentDocument(action.savedContentDocument, action.contentDocuments);
-                        break;
+                        await concreteClass.SaveContentDocument(action.savedContentDocument, action.contentDocuments);
+                        return next(action);
+                    default:
+                        return next(action);
                 }
-                return next(action);
             }
 }
--- src/pages/object-models/index.tsx
+++ src/pages/object-models/index.tsx
@@ -4,22 +4,39 @@ import { useSelector, useDispatch } from 'react-redux';
 import { REQUEST_OBJECT_MODELS } from 'Store/actions/ObjectModel';
 import { IAppState } from 'Store/State';
 
-import ObjectModelList from 'Presentation/ObjectModels/ObjectModelList';
+import guid from "uuid/v4";
+
 import Layout from 'Presentation/_Layout';
+
+import ObjectModelList from 'Presentation/ObjectModels/ObjectModelList';
+import ObjectModelListToolbar from 'Presentation/ObjectModels/object-model-list-toolbar';
+import { useHistory } from 'react-router';
+import useShortcuts from 'hooks/useShortcuts';
 
 const ObjectModelsListPage: React.FC = () => {
 
     const { objectModels } = useSelector((appState: IAppState) => appState.objectModel);
+    const history = useHistory();
     const dispatch = useDispatch();
+
+    const addObjectModel = React.useCallback(() => {
+        let id = guid().replace(/-/g, "");
+        while (objectModels.get(id)) {
+            id = guid().replace(/-/g, "");
+        }
+        history.push(`/object-models/${guid().replace(/-/g, "")}/edit`);
+    }, [objectModels, history]);
+
+    useShortcuts([{ key: 'a', action: addObjectModel }]);
+
 
     React.useEffect(() => {
         dispatch({ type: REQUEST_OBJECT_MODELS });
     }, [dispatch])
 
     return (<Layout>
-        <ObjectModelList
-            objectModels={[...objectModels.values()]}
-        />
+        <ObjectModelListToolbar addObjectModel={addObjectModel} />
+        <ObjectModelList addObjectModel={addObjectModel} objectModels={[...objectModels.values()]} />
     </Layout>);
 }
 
