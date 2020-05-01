abdbd1571f28e1f88ab3fe97924b5c76ffc91c60
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-05-02 09:57:37 +1200

    added select attachment button

--- src/components/object-models/property-types/FileUploadComponent.tsx
+++ src/components/object-models/property-types/FileUploadComponent.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
-import { Icon,  Card, Form } from 'semantic-ui-react';
+import { Icon, Card, Form, Button, Segment } from "semantic-ui-react";
 
-
-
-import { IDocumentProperty, IPropertyMap } from 'models'
-
-
+import { IDocumentProperty, IPropertyMap } from "models";
 
 interface IFileUploadComponentProps {
   documentProperty: IDocumentProperty;
@@ -19,33 +15,30 @@ interface IFileUploadComponentState {
 class FileUploadComponent extends React.Component<
   IFileUploadComponentProps,
   IFileUploadComponentState
-  > {
+> {
   public state = {
-    expanded: false
+    expanded: false,
   };
 
   public handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
+    this.setState((state) => ({ expanded: !state.expanded }));
   };
-
 
   public render() {
     const { propertyMap } = this.props;
-    return (<Card fluid={true}>
-      <Card.Content>
-        <Card.Header onClick={this.handleExpandClick}>
-          {propertyMap.name}
-        </Card.Header>
-      </Card.Content>
-      <Card.Content>
-        <Form.Field label={propertyMap.name}>
-          <label className="file-upload">
-            <Icon name="cloud upload" />
-          </label>
-        </Form.Field>
-      </Card.Content>
-    </Card>
-
+    return (
+      <Card fluid={true}>
+        <Card.Content>
+          <Card.Header onClick={this.handleExpandClick}>
+            {propertyMap.name}
+          </Card.Header>
+        </Card.Content>
+        <Card.Content>
+          <Segment placeholder>
+            <Button color="blue">Browse</Button>
+          </Segment>
+        </Card.Content>
+      </Card>
     );
   }
 }
