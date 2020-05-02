ddab8e981d3ea18c67c68a7222a57f541a1d7955
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-05-02 17:51:46 +1200

    Feature/up 8 (#10)
    * edit button
    
    * Changed to flex
    
    * added edit button to all html and meta elements

--- src/components/object-models/property-types/FileUploadComponent.tsx
+++ src/components/object-models/property-types/FileUploadComponent.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
-import { Icon,  Card, Form } from 'semantic-ui-react';
+import { Icon, Card, Form, Grid } from "semantic-ui-react";
 
-
-
-import { IDocumentProperty, IPropertyMap } from 'models'
-
-
+import { IDocumentProperty, IPropertyMap } from "models";
 
 interface IFileUploadComponentProps {
   documentProperty: IDocumentProperty;
@@ -19,33 +15,37 @@ interface IFileUploadComponentState {
 class FileUploadComponent extends React.Component<
   IFileUploadComponentProps,
   IFileUploadComponentState
-  > {
+> {
   public state = {
-    expanded: false
+    expanded: false,
   };
 
   public handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
+    this.setState((state) => ({ expanded: !state.expanded }));
   };
-
 
   public render() {
     const { propertyMap } = this.props;
-    return (<Card fluid={true}>
-      <Card.Content>
-        <Card.Header onClick={this.handleExpandClick}>
-          {propertyMap.name}
-        </Card.Header>
-      </Card.Content>
-      <Card.Content>
-        <Form.Field label={propertyMap.name}>
-          <label className="file-upload">
-            <Icon name="cloud upload" />
-          </label>
-        </Form.Field>
-      </Card.Content>
-    </Card>
-
+    return (
+      <Card fluid={true}>
+        <Card.Content>
+          <Card.Header onClick={this.handleExpandClick}>
+            <Grid columns="equal">
+              <Grid.Column>{propertyMap.name}</Grid.Column>
+              <Grid.Column style={{ flex: "0 0 auto", width: "auto" }}>
+                <Icon name="edit outline" color="blue" />
+              </Grid.Column>
+            </Grid>
+          </Card.Header>
+        </Card.Content>
+        <Card.Content>
+          <Form.Field label={propertyMap.name}>
+            <label className="file-upload">
+              <Icon name="cloud upload" />
+            </label>
+          </Form.Field>
+        </Card.Content>
+      </Card>
     );
   }
 }
--- src/components/object-models/property-types/OptionSelectComponent.tsx
+++ src/components/object-models/property-types/OptionSelectComponent.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
-import { Card, Select} from 'semantic-ui-react';
+import { Card, Select, Grid, Icon } from "semantic-ui-react";
 
-
-
-import { IPropertyMap } from 'models';
+import { IPropertyMap } from "models";
 
 interface IOptionSelectComponentProps {
   propertyMap: IPropertyMap;
@@ -16,36 +14,41 @@ interface IOptionSelectComponentState {
 class OptionSelectComponent extends React.Component<
   IOptionSelectComponentProps,
   IOptionSelectComponentState
-  > {
+> {
   public state = {
-    expanded: false
+    expanded: false,
   };
 
   public handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
+    this.setState((state) => ({ expanded: !state.expanded }));
   };
 
   public changeValue = (e: any) => {
     const { propertyMap, onPropertyUpdate } = this.props;
     const {
-      target: { name, value }
+      target: { name, value },
     } = e;
     onPropertyUpdate(Object.assign({}, propertyMap, { [name]: value }));
   };
 
   public render() {
     const { propertyMap } = this.props;
-    return (<Card fluid={true}>
-      <Card.Content>
-        <Card.Header onClick={this.handleExpandClick}>
-          {propertyMap.name}
-        </Card.Header>
-      </Card.Content>
-      <Card.Content>
-
-        <Select fluid={true} label={propertyMap.name} options={[]} />
-      </Card.Content>
-    </Card>
+    return (
+      <Card fluid={true}>
+        <Card.Content>
+          <Card.Header onClick={this.handleExpandClick}>
+            <Grid columns="equal">
+              <Grid.Column>{propertyMap.name}</Grid.Column>
+              <Grid.Column style={{ flex: "0 0 auto", width: "auto" }}>
+                <Icon name="edit outline" color="blue" />
+              </Grid.Column>
+            </Grid>
+          </Card.Header>
+        </Card.Content>
+        <Card.Content>
+          <Select fluid={true} label={propertyMap.name} options={[]} />
+        </Card.Content>
+      </Card>
     );
   }
 }
