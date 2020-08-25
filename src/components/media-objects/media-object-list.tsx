66f9cc260dba528d794842c596307a0d210fd271
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-08-25 13:56:44 +1200

    edit handling

--- src/components/media-objects/media-object-list.tsx
+++ src/components/media-objects/media-object-list.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Segment, Sidebar, Grid, Icon } from 'semantic-ui-react';
+import { Segment, Sidebar, Grid, Icon, Input } from 'semantic-ui-react';
 
 import { IMediaObject, IMediaObjectType } from 'models';
 
@@ -20,6 +20,20 @@ interface IMediaObjectListProps {
 
 const MediaObjectList: React.FC<IMediaObjectListProps> = ({ mediaObjects, selectedMediaObjectId, setSelectedMediaObject }) => {
 
+    const [editField, setEditField] = React.useState(-1);
+    const [editFieldValue, setEditFieldValue] = React.useState("");
+
+    const editButtonHandler = React.useCallback(
+        (index, value) => {
+            setEditField(index)
+            setEditFieldValue(value)
+        }, [])
+
+    const deleteButtonHandler = React.useCallback(
+        (index) => {
+
+        }, [])
+
     return (
         <>
 
@@ -39,10 +53,11 @@ const MediaObjectList: React.FC<IMediaObjectListProps> = ({ mediaObjects, select
                                         <Grid.Column className="parent" style={{ minWidth: "200px" }}>
                                             <Grid centered>
                                                 <Grid.Row>
-                                                    {object.name}
+                                                    {editField === index ?
+                                                        <Input style={{border:0,padding:0}} value={editFieldValue} onChange={(e) => { setEditFieldValue(e.target.value) }} /> : object.name}
                                                 </Grid.Row>
                                                 <Grid.Row container className="child" justify="center" style={{ width: "100%" }}>
-                                                    <Icon name='edit' style={{ fontSize: "inherit" }} />
+                                                    <Icon name='edit' style={{ cursor: "pointer", fontSize: "inherit" }} onClick={() => editButtonHandler(index, object.name)} />
                                                     <Icon name='delete' style={{ fontSize: "inherit", margin: 0 }} />
                                                 </Grid.Row>
                                             </Grid>
